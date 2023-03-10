c7032efa8ee203d2a08dfc24cae7c8bfbb8dec7d
MaouSan-19 <serkan2001.sen@gmail.com>
2023-03-10 17:11:14 +0100

    video vers commit

--- lib/details_page.dart
+++ lib/details_page.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swapi/main.dart';
 import 'package:flutter_swapi/service/api_service.dart';
-import 'VideoPlayerWidget.dart';
+import 'videoplayerwidget.dart';
 import 'package:video_player/video_player.dart';
 
-class DetailsPage extends StatefulWidget {
+/*class DetailsPage extends StatefulWidget {
   DetailsPage({Key? key, required this.character, required this.index})
       : super(key: key);
   int index;
@@ -12,6 +13,7 @@ class DetailsPage extends StatefulWidget {
   @override
   State<DetailsPage> createState() => _DetailsPageState();
 }
+
 
 class _DetailsPageState extends State<DetailsPage> {
   var planetInfo;
@@ -29,30 +31,95 @@ class _DetailsPageState extends State<DetailsPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black45,
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 22, 35, 211),
-        title: Text(
-          "${widget.character["name"]}",
-          style: TextStyle(color: Color.fromARGB(255, 209, 4, 4)),
+    return MaterialApp(
+      home:SafeArea(
+        child: Scaffold(
+         
+          body: Stack(
+             children: <Widget>[
+            
+             ],
+          ),
         ),
-        actions: [],
       ),
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image:NetworkImage(
-              'https://img.freepik.com/free-photo/3d-hyperspace-background-with-warp-tunnel-effect_1048-12526.jpg?w=996&t=st=1678099082~exp=1678099682~hmac=a3875c5a26abe84d17ba672804f275c30027dc147fd70d6af357f622d05c002a'
+    );
+  }
+}*/
+class BackgroundVideo extends StatefulWidget {
+  BackgroundVideo({Key? key, required this.character, required this.index})
+      : super(key: key);
+  int index;
+  var character;
+
+  
+  @override
+  _BackgroundVideoState createState() => _BackgroundVideoState();
+}
+
+class _BackgroundVideoState extends State<BackgroundVideo> {
+  var planetInfo;
+  
+  late VideoPlayerController _controller;
+
+  
+  @override
+  void initState() {
+
+    super.initState();
+    
+    ApisService.planetInfo("${widget.character["homeworld"]}")
+        .then((value) => {planetInfo = value, setState(() {})});
+    // Pointing the video controller to our local asset.
+    _controller = VideoPlayerController.asset("asset/star_wars_themed_light_speed_effect_blender_loop_animation.mp4");
+      _controller.initialize().then((_) {
+        // Once the video has been loaded we play the video and set looping to true.
+        _controller.play();
+        _controller.setLooping(true);
+        // Ensure the first frame is shown after the video is initialized.
+        setState(() {});
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+     
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.black45,
+          appBar: AppBar(
+            backgroundColor: Color.fromARGB(255, 22, 35, 211),
+            leading: BackButton(
+              onPressed: (
+                 Navigator.of(context).push(MaterialPageRoute(builder: (context)=>MyHomePage()));
               ),
-              fit:BoxFit.cover
+            ),
+            title: Text(
+              "${widget.character["name"]}",
+              style: TextStyle(color: Color.fromARGB(255, 209, 4, 4)),
+            ),
+            actions: [],
+          ),
+          body: Stack(
+            children: <Widget>[
+              // TODO 7: Add a SizedBox to contain our video.
+              SizedBox.expand(
+                child: FittedBox(
+                  // If your background video doesn't look right, try changing the BoxFit property.
+                  // BoxFit.fill created the look I was going for.
+                  fit: BoxFit.fill,
+                  child: SizedBox(
+                    width: _controller.value.size?.width ?? 0,
+                    height: _controller.value.size?.height ?? 0,
+                    child: VideoPlayer(_controller),
+                  ),
+                ),
               ),
-              ),
-        child: Center(
+             Center(
           child: planetInfo != null
               ? Column(
                   children: [
-                    widget.index >= 17
+                    widget.index >= 16
                         ? Image.network(
                             "https://starwars-visualguide.com/assets/img/characters/${widget.index + 2}.jpg",
                             width: MediaQuery.of(context).size.width / 2,
@@ -187,8 +254,20 @@ class _DetailsPageState extends State<DetailsPage> {
                   ],
                 )
               : CircularProgressIndicator(),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
+
+  // TODO 8: Override the dipose() method to cleanup the video controller.
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
 }
+
+// A basic login widget with a logo and a form with rounded corners.
--- lib/main.dart
+++ lib/main.dart
@@ -112,7 +112,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   Navigator.push(
                     context,
                     MaterialPageRoute(
-                      builder: (context) => DetailsPage(
+                      builder: (context) => BackgroundVideo(
                         character: listCharacters[index],
                         index: index,
                       ),
